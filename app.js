e744dc7c6020e9243f5b6f44e90336bdcbd68a62
Adam Heins <adamwheins@gmail.com>
2015-02-21 01:43:28 -0800

    Removed unnecessary files, cleaned up code.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 // mongodb dependencies.
 var mongo = require('mongodb');
@@ -34,31 +33,15 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 
-// catch 404 and forwarding to error handler
+// Catch 404 error and forward it to the error handler.
 app.use(function(req, res, next) {
-    console.log('catching 404 error');
+    console.log('Catching 404 error');
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-
-/*
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}*/
-
-/** Error handler. Provides a custom page for 404 errors. */
+// Error handler. Provides a custom page for 404 errors.
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     console.log('called error handler.');
