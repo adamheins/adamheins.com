d72d016b1b33b8390e3826dc1373eb91b2554908
Adam Heins <mail@adamheins.com>
2015-08-25 17:14:16 -0400

    Remove jQuery dependency.

--- src/public/scripts/json-resume.js
+++ src/public/scripts/json-resume.js
@@ -1,12 +1,22 @@
-var resume;
+'use strict';
 
-// Load JSON resume for use in the console.
-$(document).ready(function() {
-  $.ajax({
-    url: '/resume/resume.json',
-    dataType: 'json',
-    success: function(res) {
-      resume = res;
+/* Load JSON resume for use in the console. */
+
+function ajaxRequest(url, callback) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+      callback(xmlhttp.responseText);
     }
+  }
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+}
+
+var resume;
+window.addEventListener('load', function load(e) {
+  window.removeEventListener('load', load, false);
+  ajaxRequest('/resume/resume.json', function(res) {
+    resume = res;
   });
-});
+}, false);
