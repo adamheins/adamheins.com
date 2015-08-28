8d841415f384e7d1c8effd82ad54c8732dbd66e5
Adam Heins <mail@adamheins.com>
2015-08-28 16:09:35 -0400

    Add extensions to all shell scripts and remove obsolete ones.

--- sh/start.sh
+++ sh/start.sh
@@ -5,4 +5,5 @@ while read line; do export "$line";
 done < .env
 
 # Start the server as a daemon.
-nohup nodejs src/server.js >/dev/null 2>&1 &
+#nohup nodejs src/server.js >/dev/null 2>&1 &
+nodejs src/server.js
