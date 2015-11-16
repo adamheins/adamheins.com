96f67590e1d98911c94d9be1e292a643e7208cdb
Adam Heins <mail@adamheins.com>
2015-11-15 17:29:01 -0800

    Redirect resume page directly to official resume.

--- src/routes/resume.js
+++ src/routes/resume.js
@@ -4,18 +4,18 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-// Resume landing page, which provides links to different formats.
+// Resume landing page.
 router.get('/', function(req, res) {
-  res.render('resume/index');
+  res.redirect('/resume/resume.pdf');
 });
 
 // PDF version of my resume.
-router.get('/pdf', function(req, res) {
+router.get('/resume.pdf', function(req, res) {
   res.sendfile(path.join(__dirname, '../public/res', 'resume.pdf'));
 });
 
 // JSON version of my resume.
-router.get('/json', function(req, res) {
+router.get('/resume.json', function(req, res) {
   res.sendfile(path.join(__dirname, '../public/res', 'resume.json'));
 });
 
