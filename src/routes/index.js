e0defd54a74bbc7e73691313d51df152bfb4f5b7
Adam Heins <mail@adamheins.com>
2015-11-19 22:50:03 -0800

    Add tools page.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 var Article = require('../models/article');
 
-// Home page.
 router.get('/', function(req, res) {
   var query = Article.find().sort({date_posted: -1}).limit(3);
   query.exec(function(err, articles) {
@@ -13,19 +12,20 @@ router.get('/', function(req, res) {
   });
 });
 
-// Projects page.
 router.get('/projects', function(req, res) {
   res.render('projects');
 });
 
-// About page.
 router.get('/about', function(req, res) {
   res.render('about');
 });
 
-// pgp page.
 router.get('/pgp', function(req, res) {
   res.render('pgp');
+});
+
+router.get('/tools', function(req, res) {
+  res.render('tools');
 });
 
 module.exports = router;
