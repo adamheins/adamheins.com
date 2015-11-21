51462ba1a2c4cf496db04f416a80edc5265f69d7
Adam Heins <mail@adamheins.com>
2015-11-20 21:07:41 -0800

    Clean up unused files.

--- src/client/js/comment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Clear current comment content.
-var clearButton = document.getElementById('clear');
-if (clearButton) {
-  clearButton.addEventListener('click', function() {
-    document.getElementById('comment').value = '';
-  });
-}
