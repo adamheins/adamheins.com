48e87d91eef8e23867f1c8d0e784dfdc5c4c6eac
Adam Heins <mail@adamheins.com>
2016-02-20 01:45:46 -0500

    Individual project page routing handled by nginx.

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -8,12 +8,4 @@ router.get('/', function(req, res) {
   res.render('projects');
 });
 
-router.get('/:name', function(req, res) {
-  // For a project to get rendered correctly, it must have a file called
-  // 'index.html' in the project root.
-  var projectName = req.params.name;
-  var projectPath = path.join(__dirname, '../public/projects', projectName);
-  res.sendfile(path.join(projectPath, 'index.html'));
-});
-
 module.exports = router;
