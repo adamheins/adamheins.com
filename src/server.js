f95d406403825fd7a5421b89ffff6e9d90d045a9
Adam Heins <mail@adamheins.com>
2016-09-22 23:56:57 -0400

    Remove more unneeded deps.

--- src/server.js
+++ src/server.js
@@ -1,31 +1,25 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+let express = require('express');
+let path = require('path');
+let mongoose = require('mongoose');
 
-var router = require('./lib/router');
-var error = require('./lib/error');
+let router = require('./lib/router');
+let error = require('./lib/error');
 
 // Connect to MongoDB database.
 mongoose.connect(process.env.MONGO_URI);
 
-var app = express();
+let app = express();
 
 // View engine.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Other middleware.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-
 // Logic.
 router.route(app);
 error.handle(app);
+
 app.listen(process.env.PORT);
 
 module.exports = app;
