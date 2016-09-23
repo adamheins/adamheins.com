f716ddee50e731c41f7d7edcf08127e8e8ff85a1
Adam Heins <mail@adamheins.com>
2016-09-22 23:48:01 -0400

    Remove unneeded deps and serve favicon remotely.

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -21,8 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Other middleware.
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
