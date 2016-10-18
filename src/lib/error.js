0fb9b01e2f80b7b1a8ebaa75707486d9ececd2b2
Adam Heins <mail@adamheins.com>
2016-10-17 20:49:48 -0400

    Make 404 error explicitly the default.

--- src/lib/error.js
+++ src/lib/error.js
@@ -1,11 +1,19 @@
 'use strict';
 
+let MESSAGE_404 = 'Page not found';
+let DESC_404 = 'The page you were looking for isn\'t here! You can try heading '
+             + 'back to the <a href="/">homepage</a>.';
+
+let DESC_OTHER = 'It looks like something went wrong. You can try heading back '
+               + 'to the <a href="/">homepage</a>.';
+
 // Handle http errors.
 exports.handle = function(app) {
 
-  // Catch 404 error and forward it to the error handler.
+  // Generic handler. If the app gets this far without sending a response,
+  // assume something has gone wrong and generate a 404.
   app.use((req, res, next) => {
-    console.log('Catching 404 error');
+    console.log('Generating 404 error');
 
     let err = new Error('Not Found');
     err.status = 404;
@@ -14,22 +22,25 @@ exports.handle = function(app) {
 
   // Error handler. Provides a custom page for 404 errors.
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
     console.log('Called error handler.');
+    console.log(err);
+
+    // Errors with no status codes automatically become 404 errors.
+    if (!err.status) {
+        err.status = 404;
+    }
 
     if (err.status === 404) {
       res.render('error', {
-        status: '404',
-        message: 'Page not found',
-        description: "The page you were looking for isn't here! You can "
-                   + "try heading back to the <a href='/'>homepage</a>."
+        status: err.status,
+        message: MESSAGE_404,
+        description: DESC_404
       });
     } else {
       res.render('error', {
         status: err.status,
         message: err.message,
-        description: "It looks like something went wrong. You can try "
-                   + "heading back to the <a href='/'>homepage</a>."
+        description: DESC_OTHER
       });
     }
   });
