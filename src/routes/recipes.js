b4c8c502a0d6686425599737bb6ba5a7395170b4
Adam Heins <mail@adamheins.com>
2016-10-17 20:50:12 -0400

    Vastly simplify recipes routing.
    Take advantage of the fact that the recipes are more or less just static
    files to automatically render the correct one based on URI.

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -4,31 +4,17 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('recipes');
+    res.render('recipes');
 });
 
-router.get('/bread', (req, res) => {
-  res.render('recipes/bread');
-});
-
-router.get('/bread/basic', (req, res) => {
-  res.render('recipes/bread/basic');
-});
-
-router.get('/bread/spicy', (req, res) => {
-  res.render('recipes/bread/spicy');
-});
-
-router.get('/bread/cinnamon', (req, res) => {
-  res.render('recipes/bread/cinnamon');
-});
-
-router.get('/no-regrets-french-toast', (req, res) => {
-  res.render('recipes/no-regrets-french-toast');
-});
-
-router.get('/gin-blitz', (req, res) => {
-  res.render('recipes/gin-blitz');
+router.get('/*', (req, res, next) => {
+    res.render('recipes' + req.path, {}, (err, html) => {
+        if (err) {
+            next();
+        } else {
+            res.send(html);
+        }
+    });
 });
 
 module.exports = router;
