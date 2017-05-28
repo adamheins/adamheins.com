259fa1c5bf89df039735e1cd1706b10da2192a39
Adam Heins <mail@adamheins.com>
2017-05-28 19:06:10 -0400

    Clear out all existing source.

--- sh/backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Back up mongodb database of articles.
-mongodump --db adamheins --out ~/backups/$(date +%F_%s) --collection posts
--- sh/prestart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Start mongo.
-if pgrep mongod >/dev/null; then
-  echo "mongod process already running, continuing..."
-else
-  mongod --fork --config /etc/mongodb.conf
-fi
-
