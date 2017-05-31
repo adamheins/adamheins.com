03f7a159035447243372ed58363360afde69eeb5
Adam Heins <mail@adamheins.com>
2017-05-30 22:01:49 -0400

    Add configuration file for the compiler.

--- compile.js
+++ compile.js
@@ -18,18 +18,13 @@ let resolve = require('./lib/resolve');
 const PRETTY_DATE_FORMAT = 'MMMM D, YYYY';
 const REQUIRED_FIELDS = ['title', 'date', 'link', 'flavour', 'description',
                          'file', 'scripts', 'styles', 'private'];
-const STATIC_HOST = 'https://static.adamheins.com';
-
-const TEMPLATE_PATH = 'templates';
-const ARTICLES_PATH = 'articles';
-const PUBLIC_PATH = 'public';
-
-const ARTICLE_TEMPLATE_PATH = path.join(TEMPLATE_PATH, 'blog/article.pug');
-const ARTICLE_PUBLIC_DIR = path.join(PUBLIC_PATH, 'blog');
-
-const ARTICLES_GLOB = ARTICLES_PATH + '/**/*.yaml';
-const TEMPLATE_GLOB = TEMPLATE_PATH + '/**/*.pug'
 const TEMPLATE_IGNORE = ['**/mixins/*', '**/includes/*', '**/article.pug'];
+const CONFIG_PATH = 'config.yaml';
+
+
+function loadConfig(configPath) {
+    return yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+}
 
 
 // Checks the article data to ensure all required fields exist.
@@ -57,8 +52,8 @@ function validateArticleData(file, data) {
 }
 
 
-function templateToPublic(file, html) {
-    let htmlFile = file.replace(TEMPLATE_PATH, PUBLIC_PATH)
+function templateToPublic(file, html, config) {
+    let htmlFile = file.replace(config.paths.templates, config.paths.public)
                        .replace('.pug', '.html');
     let htmlDir = path.dirname(htmlFile)
     mkdirp.sync(htmlDir);
@@ -67,9 +62,11 @@ function templateToPublic(file, html) {
 
 
 // Parse articles from yaml and markdown files.
-function parseArticles() {
+function parseArticles(config) {
     let articles = [];
-    glob.sync(ARTICLES_GLOB).forEach(file => {
+    let articlesGlob = config.paths.articles + '/**/*.yaml';
+
+    glob.sync(articlesGlob).forEach(file => {
         let data = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
         let valid = validateArticleData(file, data);
 
@@ -90,6 +87,11 @@ function parseArticles() {
 
         data.scripts = data.scripts.map(resolve.script);
         data.styles = data.styles.map(resolve.style);
+
+        data.fileName = data.link + '.html';
+        if (config.local) {
+            data.link = data.link + '.html';
+        }
 
         // Format date.
         data.date = moment(data.date);
@@ -113,34 +115,46 @@ function parseArticles() {
 
 
 // Compile pug template files to html files.
-function renderTemplates(articles) {
+function renderTemplates(articles, config) {
+    let localHost = path.join(fs.realpathSync('.'), config.paths.public)
+    let host = config.local ? localHost : config.hosts.host;
+    let year = moment().format('YYYY');
+
     let pugOptions = {
-        basedir: TEMPLATE_PATH
+        basedir: config.paths.templates
     };
 
     let pugLocals = {
-        articles: articles,
-        articles3: articles.slice(0, 3),
-        staticHost: STATIC_HOST,
-        year: moment().format('YYYY')
+        articles:   articles,
+        articles3:  articles.slice(0, 3),
+        host:       host,
+        staticHost: config.hosts.static,
+        year:       year
     }
 
     // Render non-article templates.
-    glob.sync(TEMPLATE_GLOB, { ignore: TEMPLATE_IGNORE }).forEach(file => {
+    let templateGlob = config.paths.templates + '/**/*.pug'
+    glob.sync(templateGlob, { ignore: TEMPLATE_IGNORE }).forEach(file => {
         let html = pug.renderFile(file, merge(pugOptions, pugLocals));
-        templateToPublic(file, html);
+        templateToPublic(file, html, config);
     });
 
     // Render each article.
-    let articleFunc = pug.compileFile(ARTICLE_TEMPLATE_PATH, pugOptions);
+    let articleTemplatePath = path.join(config.paths.templates,
+                                        'blog/article.pug');
+    let articleFunc = pug.compileFile(articleTemplatePath, pugOptions);
+
     articles.forEach(article => {
         let options = {
-            article: article,
-            doctype: 'html',
-            moment: moment,
-            staticHost: STATIC_HOST
+            article:    article,
+            host:       host,
+            moment:     moment,
+            staticHost: config.hosts.static,
+            year:       year
         };
-        let file = path.join(ARTICLE_PUBLIC_DIR, article.link + '.html');
+
+        let articlePublicDir = path.join(config.paths.public, 'blog');
+        let file = path.join(articlePublicDir, article.fileName);
         let html = articleFunc(options);
         fs.writeFileSync(file, html);
     });
@@ -148,9 +162,10 @@ function renderTemplates(articles) {
 
 
 function main() {
-    let articles = parseArticles();
+    let config = loadConfig(CONFIG_PATH);
+    let articles = parseArticles(config);
     console.log(articles.length + ' articles rendered.');
-    renderTemplates(articles);
+    renderTemplates(articles, config);
 }
 
 main();
