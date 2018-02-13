6a193872f307648985145d2e5cbbd0ed6592fff9
Adam Heins <mail@adamheins.com>
2018-02-13 09:40:20 -0500

    Cludgy single article rendering functionality.

--- blogger.js
+++ blogger.js
@@ -24,7 +24,16 @@ const CONFIG_PATH = 'config.yaml';
 
 
 function loadConfig(configPath) {
-    return yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+    let config = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+
+    let templateRoot = config.paths.templates.root;
+    let templateArticles = config.paths.templates.article;
+    let templateProjects = config.paths.templates.projects;
+
+    config.paths.templates.article = path.join(templateRoot, templateArticles);
+    config.paths.templates.projects = path.join(templateRoot, templateProjects);
+
+    return config
 }
 
 
@@ -63,43 +72,49 @@ function templateToPublic(file, html, config) {
 }
 
 
+// Parse a single article.
+function parseArticle(config, data_file) {
+    let data = yaml.safeLoad(fs.readFileSync(data_file, 'utf8'));
+    let valid = validateArticleData(data_file, data);
+
+    // If the article is not valid, compilation fails.
+    if (!valid) {
+        process.exit(1);
+    }
+
+    // Skip this file if it is marked as private.
+    if (data.private) {
+        return;
+    }
+
+    // Parse body markdown file.
+    let dirname = path.dirname(data_file);
+    let bodyFile = path.join(dirname, data.file);
+    data.html = md.markdown(fs.readFileSync(bodyFile, 'utf8'));
+
+    data.scripts = data.scripts.map(resolve.script);
+    data.styles = data.styles.map(resolve.style);
+
+    data.fileName = data.link + '.html';
+    if (!config.prod) {
+        data.link = data.link + '.html';
+    }
+
+    // Format date.
+    data.date = moment(data.date);
+    data.prettyDate = data.date.local().format(PRETTY_DATE_FORMAT);
+
+    return data;
+}
+
+
 // Parse articles from yaml and markdown files.
 function parseArticles(config) {
     let articles = [];
     let articlesGlob = config.paths.articles + '/**/*.yaml';
 
     glob.sync(articlesGlob, { ignore: ['drafts/*'] }).forEach(file => {
-        let data = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-        let valid = validateArticleData(file, data);
-
-        // If the article is not valid, compilation fails.
-        if (!valid) {
-            process.exit(1);
-        }
-
-        // Skip this file if it is marked as private.
-        if (data.private) {
-            return;
-        }
-
-        // Parse body markdown file.
-        let dirname = path.dirname(file);
-        let bodyFile = path.join(dirname, data.file);
-        data.html = md.markdown(fs.readFileSync(bodyFile, 'utf8'));
-
-        data.scripts = data.scripts.map(resolve.script);
-        data.styles = data.styles.map(resolve.style);
-
-        data.fileName = data.link + '.html';
-        if (!config.prod) {
-            data.link = data.link + '.html';
-        }
-
-        // Format date.
-        data.date = moment(data.date);
-        data.prettyDate = data.date.local().format(PRETTY_DATE_FORMAT);
-
-        articles.push(data);
+        articles.push(parseArticle(config, file));
     });
 
     // Sort articles by date in descending order.
@@ -116,41 +131,6 @@ function parseArticles(config) {
 }
 
 
-function validateProjectData(data) {
-    return true;
-}
-
-
-function parseProjects(config) {
-    let data = yaml.safeLoad(fs.readFileSync(config.paths.projects, 'utf8'));
-
-    data.forEach(section => {
-        section.projects.forEach(project => {
-            let links = project.links;
-
-            // Reform the links list into an object that allows for easier
-            // templating.
-            if (links.length > 0) {
-                project.links = {
-                    notLast: links.slice(0, -1),
-                    last: links.slice(-1)[0],
-                };
-            } else {
-                project.links = {
-                    notLast: [],
-                };
-            }
-        });
-    });
-
-    if (!validateProjectData(data)) {
-        process.exit(1);
-    }
-
-    return data;
-}
-
-
 function renderArticles(articles, config, pugOptions) {
     let articleFunc = pug.compileFile(config.paths.templates.article,
                                       pugOptions);
@@ -161,7 +141,8 @@ function renderArticles(articles, config, pugOptions) {
             moment:     moment,
         });
 
-        let articlePublicDir = path.join(config.paths.public, 'blog');
+        // TODO need to fix before merge
+        let articlePublicDir = config.paths.public;
         let file = path.join(articlePublicDir, article.fileName);
         let html = articleFunc(options);
         fs.writeFileSync(file, html);
@@ -169,18 +150,8 @@ function renderArticles(articles, config, pugOptions) {
 }
 
 
-function renderProjects(projectData, config, pugOptions) {
-    let options = merge(pugOptions, { sections: projectData });
-    let html = pug.renderFile(config.paths.templates.projects, options);
-
-    let outFile = path.join(config.paths.public, 'projects', 'index.html');
-    mkdirp.sync(path.dirname(outFile));
-    fs.writeFileSync(outFile, html);
-}
-
-
 // Compile pug template files to html files.
-function renderTemplates(articles, projectData, config) {
+function renderTemplates(articles, config) {
     let localHost = path.join(fs.realpathSync('.'), config.paths.public)
     let host = config.prod ? config.hosts.host : localHost;
     let year = moment().format('YYYY');
@@ -205,7 +176,28 @@ function renderTemplates(articles, projectData, config) {
     });
 
     renderArticles(articles, config, pugOptions);
-    renderProjects(projectData, config, pugOptions);
+}
+
+function one() {
+    if (process.argv.length < 3) {
+        console.log('Usage: blogger data.yaml');
+        return 1;
+    }
+
+    let config = loadConfig(CONFIG_PATH);
+
+    let data_file = process.argv[2];
+
+    let article = parseArticle(config, data_file);
+
+    let pugOptions = {
+        basedir:    config.paths.templates.root,
+        host:       config.hosts.host,
+        staticHost: config.hosts.static,
+        year:       moment().format('YYYY')
+    };
+
+    renderArticles([article], config, pugOptions);
 }
 
 
@@ -229,12 +221,10 @@ function main() {
     }
 
     let articles = parseArticles(config);
-    let projectData = parseProjects(config);
-
-    renderTemplates(articles, projectData, config);
+    renderTemplates(articles, config);
 
     console.log(articles.length + ' articles rendered.');
-    console.log('Projects rendered.');
 }
 
-main();
+// main();
+one();
